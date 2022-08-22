6a090d61ae15a887573ec2f9c3bf3e2d4b728ed0
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-08-22 16:08:48 +0300

    Update moduleD.py

--- dictionary/moduleD.py
+++ dictionary/moduleD.py
@@ -1,13 +1,13 @@
 d = {"car":"авто", "house":"дім", "road":"дорога","language":"мова"}
 def Translate_word():
     a = input("Input word by English to translate: ")
-    a = d.get(a)#to translate (get value by key)
+    a = d.get(a) #to translate (get value by key)
     print(a)
 
 def Add_word():
     a = input("Input word by English: ")
     b = input("Input word by Ukrainian: ")
-    d.setdefault(a,b)#add in console but not in dict!!! use d.update()
+    d.setdefault(a,b) #add in console but not in dict!!! use d.update()
     print("Add one")
     print(d)
            
