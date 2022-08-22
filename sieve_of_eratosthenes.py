ff38d9adf57c69dd9ae115e500623bffedae93df
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-08-22 14:37:10 +0300

    Update and rename 12) Решето Ератосфена.py to sieve_of_eratosthenes.py

---
+++ sieve_of_eratosthenes.py
@@ -0,0 +1,9 @@
+A=[True]*150
+A[0]=A[1]=False
+for k in range(2,150):
+  if A[k]:#
+    for m in range(k*k,150,k):
+     A[m]=False
+for k in range(150):#
+  print(k,"-","просте" if A[k] else"складене")
+  
