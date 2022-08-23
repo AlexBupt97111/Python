836de324c41cfcf1cb2be6b70a3a4fcd215cdee3
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-08-23 16:50:53 +0300

    Create find_oldest.py

---
+++ find_oldest.py
@@ -0,0 +1,14 @@
+import random
+names = ["Simba", "Murchik", "Murka", "Masya", "Pushok"]
+cats  = [] 
+for i in range(5): 
+    cat = {"name":names[i],
+           "age": random.randint(1,15) 
+          }
+    cats.append(cat) 
+print(cats) 
+max_cat = cats[0] 
+for cat in cats:
+    if max_cat.get("age") < cat.get("age"):
+        max_cat = cat
+print(max_cat)
