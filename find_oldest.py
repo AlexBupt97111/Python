66d3d0b199203f65d4207163c0ab33c98586b342
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-09-19 13:44:41 +0300

    Update find_oldest.py

--- find_oldest.py
+++ find_oldest.py
@@ -2,11 +2,12 @@ import random
 names = ["Simba", "Murchik", "Murka", "Masya", "Pushok"]
 cats  = [] 
 for i in range(5): 
-    cat = {"name":names[i],
-           "age": random.randint(1,15) 
+    cat = {"name": names[i],
+           "age": random.randint(1, 15) 
           }
     cats.append(cat) 
-print(cats) 
+print(cats)
+
 max_cat = cats[0] 
 for cat in cats:
     if max_cat.get("age") < cat.get("age"):
