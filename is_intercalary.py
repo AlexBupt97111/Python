3bd1f6f509d3d315e8d3fdf059715d613a849cd1
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-09-19 13:35:45 +0300

    Update is_intercalary.py

--- is_intercalary.py
+++ is_intercalary.py
@@ -1,7 +1,10 @@
-year = int(input("Input year for checking: "))
-if year % 4 == 0  or year % 400 == 0:
-    print( year," - intercalary" )
-elif year % 100 == 0:
-    print (year, " - not intercalary")   
-else:
-    print (year, " - not intercalary")
+def check_year():
+    year = int(input("Input year for checking: "))
+    if year % 4 == 0  or year % 400 == 0:
+        print( year," - intercalary" )
+    elif year % 100 == 0:
+        print (year, " - not intercalary")   
+    else:
+        print (year, " - not intercalary")
+        
+check_year()
