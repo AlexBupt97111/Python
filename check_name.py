7d2f283f783c75a0fb1bc2bfd643a95b50efca21
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-09-19 11:49:09 +0300

    Update check_name.py

--- check_name.py
+++ check_name.py
@@ -1,5 +1,8 @@
-a = input("Input name: ")
-if a.isalpha() and a.istitle():
-    print("Ok")
-else:
-    print("Some mistakes. Check.")
+def check_name():
+    name = input("Input name: ")
+    if name.isalpha() and name.istitle():
+        print("Ok")
+    else:
+        print("Some mistakes. Check it.")
+                
+check_name()
