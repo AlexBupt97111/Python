56fa3ec6bd86f54803d6baf148f9670b824c9617
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-09-19 12:40:53 +0300

    Update criss_cross.py

--- criss_cross.py
+++ criss_cross.py
@@ -9,11 +9,11 @@ def create_field(line, colmn, symb): #creating two-dimensional array;
         field.append(internal)
     return field 
 "_"
-F = create_field(3,3,"_") #3 - size of field: "_" - temporary game symbol
-                          #gamefield - global variable; symb"_" - required positional argument
+Field = create_field(3,3,"_") #3 - size of field: "_" - temporary game symbol
+                              #gamefield - global variable; symb"_" - required positional argument
 
-def show_field(F):
-    for el in F:
+def show_field(Field):
+    for el in Field:
         print(*el)
                 
 def goPlayer(symb):
@@ -21,16 +21,16 @@ def goPlayer(symb):
     while gou != "go":
     
         (in_line, in_column) = input("input raw(digits 1-3), column(digits 1-3): ").replace(",", " ").split()
-        x,y = int(in_line)-1, int(in_column)-1 
+        x, y = int(in_line)-1, int(in_column)-1 
         
         if x not in range(0,3) or y not in range(0,3):
             print("\n Uncorrect coordinates \n")
         else:    
-            if F[x][y] =="_":
-                F[x][y] = symb  
+            if Field[x][y] =="_":
+                Field[x][y] = symb  
                 
                 gou = "go"
-                show_field(F)
+                show_field(Field)
                 print()
                 return x, y 
                                
@@ -42,20 +42,20 @@ def goComp(symb): #condition for computer
     gou = ""
     
     while gou != "go": 
-        x = rng.randrange(len(F))
-        y = rng.randrange(len(F[0])) 
+        x = rng.randrange(len(Field))
+        y = rng.randrange(len(Field[0])) 
         
-        if F[x][y]!="_":
+        if Field[x][y]!="_":
             continue
         else:
-            F[x][y]= symb
+            Field[x][y]= symb
             gou = "go"  
-            show_field(F)
+            show_field(Field)
             print()
     return x,y
     
-def ifFinish( x,y, symb):
-    if(F[x][0]==F[x][1]==F[x][2]==symb)or(F[0][y]==F[1][y]==F[2][y]==symb)or(F[0][0]==F[1][1]==F[2][2]==symb)or(F[0][-1]==F[1][-2]==F[2][0]==symb):
+def ifFinish(x, y, symb):
+    if(Field[x][0] == Field[x][1] == Field[x][2] == symb) or (Field[0][y] == Field[1][y] == Field[2][y] == symb) or (Field[0][0] == Field[1][1] == Field[2][2] == symb) or (Field[0][-1] == Field[1][-2] == Field[2][0] == symb):
         return 1
     else:
         return 0
@@ -65,29 +65,29 @@ def createGame():
     comp = "+" if symb =="0" else "0"
 
     win = False
-    count = len(F)*len(F[0])
+    count = len(Field) * len(Field[0])
     
     while win == False and count>0:
 
         if symb=="+": #if player chose + - he goes first
             count-=1
-            x,y = goPlayer(symb) #input symbol in position of Player(coordinates x;y)
-            win += ifFinish(x,y, symb)
+            x, y = goPlayer(symb) #input symbol in position of Player(coordinates x;y)
+            win += ifFinish(x, y, symb)
 
             if win == False and count>0:
                 count-=1        
-                x,y = goComp(comp)
-                win += ifFinish(x,y, comp)
+                x, y = goComp(comp)
+                win += ifFinish(x, y, comp)
 
         if symb=="0": 
             count-=1                    
-            x,y = goComp(comp)
+            x, y = goComp(comp)
             win += ifFinish(x,y,symb=comp)        
             
             if win == False and count>0:
-                count-=1
-                x,y = goPlayer(symb)
-                win += ifFinish(x,y, symb)  
+                count -= 1
+                x, y = goPlayer(symb)
+                win += ifFinish(x, y, symb)  
 
-    print("Game over, win %s"% F[x][y]) if win != False else print("Battle draw")
+    print("Game over, win %s"% Field[x][y]) if win != False else print("Battle draw")
 createGame()
