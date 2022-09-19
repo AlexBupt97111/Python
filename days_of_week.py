e18bc8431efb496823edaf25851d93529b53da71
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-09-19 12:59:51 +0300

    Update days_of_week.py

--- days_of_week.py
+++ days_of_week.py
@@ -1,11 +1,14 @@
-d = {1:"Monday",
-     2:"Tuesday",
-     3:"Wednesday",
-     4:"Thursday",
-     5:"Friday",
-     6:"Saturday",
-     7:"Sunday"
+def name_of_the_day():
+    d = {1:"Monday",
+         2:"Tuesday",
+         3:"Wednesday",
+         4:"Thursday",
+         5:"Friday",
+         6:"Saturday",
+         7:"Sunday"
   
 }
-num = int(input("Input number in range 1-7: "))
-print(num," =>",  d.get(num))
+    num = int(input("Input number in range 1-7: "))
+    print(num," =>",  d.get(num))
+
+name_of_the_day()
