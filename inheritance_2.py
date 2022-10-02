427e626978420068c50beda5e049bb418968351f
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-10-02 16:52:47 +0300

    Update and rename multiple_inheritance_2.py to inheritance_2.py

--- inheritance_2.py
+++ inheritance_2.py
@@ -4,19 +4,18 @@ class Table:
   
 
 class RectT(Table):
-    
     def SquareR(self):
         length, width  = self.v
         return length*width
 
 class OvalT(Table):
-    
     def SquareO(self):
         r, = self.v
         return 3.14*r**2
     
     
 t = RectT(10,16)
-print(t.SquareR())#call always with ()  - without () will show bouund method!!
+print(t.SquareR()) #call always with ()  - without () will show bouund method!!
+
 t2 = OvalT(5.6)
 print(t2.SquareO())
