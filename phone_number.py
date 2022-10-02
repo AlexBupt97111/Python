88356b1cfd474ce4bfdd274eb686a61c47c9e510
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-10-02 15:40:43 +0300

    Update phone_number.py

--- phone_number.py
+++ phone_number.py
@@ -1,8 +1,8 @@
 def check_number():
-    number = input("Input phone number:\n")
+    number = input("Enter the phone number in full, including symbol +"" :\n")
     temp = number.replace('(' ,'').replace(')', '').replace('+', '') 
     temp_p = temp.isdigit()
-    if (number[0]=='+') or (number[0]=='3') and len(temp)==12:
+    if (number[0] == '+') and (number[1] == '3', number[2] == '8', number[3] == '0') and len(temp) == 12:
         print("Ukrainian phone number")
     else:
 	print("Number of other country")
