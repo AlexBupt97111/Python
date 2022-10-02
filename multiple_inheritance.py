2a2170c200e30b155e69c2b8b24cc8f64374f424
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-10-02 16:27:46 +0300

    Update multiple_inheritance.py

--- multiple_inheritance.py
+++ multiple_inheritance.py
@@ -1,36 +1,79 @@
-class Point:
-    def __init__(self, x = 0, y = 0):
-        self.__x = x
-        self.__y = y
-        
-    def __str__(self): 
-        return f"({self.__x}, {self.__y})"
-#class Pos:
-    #def __init__(self):
-        #print("Constructor Pos")
-        #super().__init__()
-         
-class Styles:
-    def __init__(self):
-        print("Constructor Styles")
-        super().__init__()
-
-class Pos:
-    def __init__(self):
-        print("Constructor Pos")
-        super().__init__() #use super - no matter which order
-
-class Line(Pos, Styles): #change places of classes  - and norm (adapted)
-    def __init__(self, sp:Point, ep:Point,color = "red", width = 1): #write args here
-        self._sp = sp
-        self._ep = ep
-        self._color = color
-        self._width = width
+class AMD:
+    def __init__ (self):
+        print("Constructor AMD")
         super().__init__()
         
-    def draw(self):
-        print(f"Painting of line: {self._sp}, {self._ep}, {self._color}, {self._width}")
+class Intel:
+    def __init__(self):
+        print("Constructor Intel")
+        super().__init__()
+        
+class NVidia:
+    def __init__(self):
+        print("Constructor NVidia")
+        super().__init__()
+        
+class GeForce:
+    def __init__(self):
+        print("Constructor GeForce")
+        super().__init__()
 
-l = Line(Point(10, 10), Point(100, 100), "green", 5)
-l.draw() #call method of class Line
-print(Line.__mro__)
+class CPU(AMD, Intel):
+    print("Constructor CPU")
+    def __init__(self):
+        super().__init__()
+
+    def setProcessor(self, processor: str, cpuCores: int = None, cpuFrequency: float = None):#setter
+        if cpuCores is None and cpuFrequency is None:
+            self._processor = processor
+        else:
+            self._processor = processor
+            self._cpuCores = cpuCores
+            self._cpuFrequency = cpuFrequency#use setter
+        
+class GPU (NVidia, GeForce):
+    print("Constructor GPU")
+    def __init__(self):
+        super().__init__()# if use super().
+
+    def setVideoCard(self, videoCard: str, gpuCores: int = None, gpuMemory: int = None):
+        if gpuCores is None and gpuMemory is None:
+            self._videoCard = videoCard
+        else:
+            self._videoCard = videoCard
+            self._gpuCores = gpuCores
+            self._gpuMemory = gpuMemory
+class Memory:
+    def __init__(self):
+        print("Constructor Memory")
+        super().__init__()
+
+def setMemory(self, memoryVolume: int, memoryFrequency: int):#setter
+        self._memoryVolume = memoryVolume
+        self._memoryFrequency = memoryFrequency
+
+
+class Motherboard(CPU,GPU,Memory):
+    def __init__(self,processor: str, cpuCores: int, cpuFrequency: float, \
+                 videoCard: str, gpuCores: int, gpuMemory: int, \
+                 memoryVolume: int, memoryFrequency: int):
+        self._processor = processor
+        self._cpuCores = cpuCores
+        self._cpuFrequency = cpuFrequency
+        self._videoCard = videoCard
+        self._gpuCores = gpuCores
+        self._gpuMemory = gpuMemory
+        self._memoryVolume = memoryVolume
+        self._memoryFrequency = memoryFrequency
+        super().__init__()
+
+    def showInfo(self):
+        print(f"Processor: {self._processor}\n\
+CPU: cores - {self._cpuCores}, frequency - {self._cpuFrequency}\n\
+Video Card: {self._videoCard}\n\
+GPU: cores - {self._gpuCores}, memory - {self._gpuMemory}\n\
+RAM: volume - {self._memoryVolume}, frequency - {self._memoryFrequency}")
+
+m = Motherboard("AMD Ryzen 7 2700x", 8, 3.7, "GeForce 1080 Ti", 3584, 8, 8, 3200)
+
+m.showInfo()
