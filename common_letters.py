e111135e259ee9e461e9e98c3b70ecac8ea64106
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-10-02 15:43:13 +0300

    Update common_letters.py

--- common_letters.py
+++ common_letters.py
@@ -1,7 +1,6 @@
 def common():
     sentence_1 = set(input("Input first sentence: "))
     sentence_2 = set(input("Input second sentence: "))
-    #print(sentence_1, sentence_2)
     print(sentence_1.intersection(sentence_2))
 
 common()
